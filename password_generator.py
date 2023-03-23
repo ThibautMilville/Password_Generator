1fcba792783b1af1b823ca564a50922eb28b8fdb
ThibautMilville <tmilville.pro@gmail.com>
2023-03-23 20:42:16 +0100

    end

--- password_generator.py
+++ password_generator.py
@@ -35,7 +35,7 @@ def generate_password(min_length, numbers=True, special_characters=True):
     return pwd
 
 min_length = int(input("Enter the minimum length : "))
-has_number = input("Do you want to have numbers (y/n)?").lower == "y"
-has_special = input("Do you want to have special characters (y/n)?").lower == "y"
+has_number = input("Do you want to have numbers (y/n)? ").lower() == "y"
+has_special = input("Do you want to have special characters (y/n)? ").lower() == "y"
 pwd = generate_password(min_length, has_number, has_special)
-print(pwd)
+print("The generated password is: ", pwd)
