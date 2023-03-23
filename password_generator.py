6593b836ab72e01ca14ece09e8b417816a5cd41f
ThibautMilville <tmilville.pro@gmail.com>
2023-03-23 20:15:53 +0100

    password_generator

--- password_generator.py
+++ password_generator.py
@@ -12,4 +12,26 @@ def generate_password(min_length, numbers=True, special_characters=True):
     if special_characters:
         characters += special
 
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+
+    return pwd
+
 generate_password(10)
