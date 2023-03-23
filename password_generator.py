9966968990c87c1843f3596090d8a5264dce9c2d
ThibautMilville <tmilville.pro@gmail.com>
2023-03-23 20:00:02 +0100

    start

---
+++ password_generator.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def generate_password(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    
+
+generate_password(10)
